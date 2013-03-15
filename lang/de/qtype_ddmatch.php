9e6366aae734d652fc500bba819cbbd5b6cfc57f
Brian King <brian@liip.ch>
2013-03-15 11:44:03 +0100

    added updated german language file

---
+++ lang/de/qtype_ddmatch.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * The language strings for the match question type.
+ *
+ * @package    qtype_ddmatch
+ * @copyright Adriane Boyd (adrianeboyd@gmail.com)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+$string['pluginname'] = 'Drag-and-Drop-Zuordnung';
+$string['draganswerhere'] = 'Ziehen Sie mit der Maus die Antwort an die richtige Stelle in diesem Bereich';
+$string['editingddmatch'] = 'Drag-and-Drop Zuordnungsfrage bearbeiten';
+$string['pluginname_help'] = 'Bei Drag-and-drop matching question müssen die Benutzer eine Liste von Namen oder Aussagen (Fragen) korrekt mit einer zweiten Liste von Namen oder Aussagen (Antworten) verbinden.'
+$string['pluginname_link'] = 'question/type/ddmatch';
+$string['pluginnameadding'] = 'Drag-and-Drop Zuordnungsfrage hinzufügen';
+$string['pluginnameediting'] = 'Drag-and-Drop Zuordnungsfrage bearbeiten';
+$string['pluginnamesummary'] = 'Eine Erweiterung der Zuordnungsfrage, die dem Benutzer ermöglicht, Antworte per Drag-and-Drop zuzuordnen.';
