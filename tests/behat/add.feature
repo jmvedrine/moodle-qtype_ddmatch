ca5ec375eee053b4dbcc6c0f2d6eef47d347ac55
Jean-Michel Vedrine <vedrine@vedrine.org>
2018-04-03 11:33:42 +0200

    First Behat tests

---
+++ tests/behat/add.feature
@@ -0,0 +1,37 @@
+@qtype @qtype_ddmatch
+Feature: Test creating a drag and drop matching question
+  As a teacher
+  In order to test my students
+  I need to be able to create a drag and drop matching question
+
+  Background:
+    Given the following "users" exist:
+      | username | firstname | lastname | email               |
+      | teacher1 | T1        | Teacher1 | teacher1@moodle.com |
+    And the following "courses" exist:
+      | fullname | shortname | category |
+      | Course 1 | C1        | 0        |
+    And the following "course enrolments" exist:
+      | user     | course | role           |
+      | teacher1 | C1     | editingteacher |
+    And I log in as "teacher1"
+    And I am on "Course 1" course homepage
+    And I navigate to "Question bank" node in "Course administration"
+
+  Scenario: Create a drag and drop matching question
+    When I add a " Drag-and-Drop Matching" question filling the form with:
+      | Question name                      | ddmatch-001                                    |
+      | Question text                      | Match the country with the capital city.       |
+      | General feedback                   | England=London, France=Paris and Spain=Madrid. |
+      | id_subquestions_0                  | England                                        |
+      | id_subanswers_0                    | London                                         |
+      | id_subquestions_1                  | France                                         |
+      | id_subanswers_1                    | Paris                                          |
+      | id_subquestions_2                  | Spain                                          |
+      | id_subanswers_2                    | Madrid                                         |
+      | For any correct response           | Your answer is correct                         |
+      | For any partially correct response | Your answer is partially correct               |
+      | For any incorrect response         | Your answer is incorrect                       |
+      | Hint 1                             | This is your first hint                        |
+      | Hint 2                             | This is your second hint                       |
+    Then I should see "ddmatch-001"
--- tests/helper.php
+++ tests/helper.php
@@ -17,9 +17,8 @@
 /**
  * Contains the helper class for the select missing words question type tests.
  *
- * @package    qtype
- * @subpackage ddmatch
- * @copyright  2011 The Open University
+ * @package    qtype_ddmatch
+ * @copyright  2018 Jean-Michel Vedrine
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -30,10 +29,14 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Test helper class for the drag and rop matching question type.
  *
- * @copyright  2011 The Open University
+ * @copyright  2018 Jean-Michel Vedrine
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class qtype_ddmatch_test_helper {
+class qtype_match_test_helper extends question_test_helper {
+    public function get_test_questions() {
+        return array('foursubq');
+    }
+
     /**
      * Makes a matching question to classify 'Dog', 'Frog', 'Toad' and 'Cat' as
      * 'Mammal', 'Amphibian' or 'Insect'.
@@ -66,4 +69,92 @@ class qtype_ddmatch_test_helper {
 
         return $ddmatch;
     }
+
+    /**
+     * Makes a drag and drop matching question about classing some animals.
+     * @return object the question definition data, as it might be returned from
+     * get_question_options.
+     */
+    public function get_ddmatch_question_data_foursubq() {
+        global $USER;
+        $q = new stdClass();
+        test_question_maker::initialise_question_data($q);
+        $q->name = 'Drag and drop matching question';
+        $q->qtype = 'ddmatch';
+        $q->parent = 0;
+        $q->questiontext = 'Classify the animals.';
+        $q->questiontextformat = FORMAT_HTML;
+        $q->generalfeedback = 'General feedback.';
+        $q->generalfeedbackformat = FORMAT_HTML;
+        $q->defaultmark = 1;
+        $q->penalty = 0.3333333;
+        $q->length = 1;
+        $q->hidden = 0;
+        $q->createdby = $USER->id;
+        $q->modifiedby = $USER->id;
+
+        $q->options = new stdClass();
+        $q->options->shuffleanswers = 1;
+        test_question_maker::set_standard_combined_feedback_fields($q->options);
+
+        $q->options->subquestions = array(
+            14 => (object) array(
+                'id' => 14,
+                'questiontext' => 'frog',
+                'questiontextformat' => FORMAT_HTML,
+                'answertext' => 'amphibian'),
+            15 => (object) array(
+                'id' => 15,
+                'questiontext' => 'cat',
+                'questiontextformat' => FORMAT_HTML,
+                'answertext' => 'mammal'),
+            16 => (object) array(
+                'id' => 16,
+                'questiontext' => 'newt',
+                'questiontextformat' => FORMAT_HTML,
+                'answertext' => 'amphibian'),
+            17 => (object) array(
+                'id' => 17,
+                'questiontext' => '',
+                'questiontextformat' => FORMAT_HTML,
+                'answertext' => 'insect'),
+        );
+
+        return $q;
+    }
+
+    /**
+     * Makes a drag and drop matching question about classing some animals.
+     * @return object the question definition data, as it might be returned from
+     *      the question editing form.
+     */
+    public function get_ddmatch_question_form_data_foursubq() {
+        $q = new stdClass();
+        $q->name = 'Drag and drop matching question';
+        $q->questiontext = array('text' => 'Classify the animals.', 'format' => FORMAT_HTML);
+        $q->generalfeedback = array('text' => 'General feedback.', 'format' => FORMAT_HTML);
+        $q->defaultmark = 1;
+        $q->penalty = 0.3333333;
+
+        $q->shuffleanswers = 1;
+        test_question_maker::set_standard_combined_feedback_form_data($q);
+
+        $q->subquestions = array(
+            0 => array('text' => 'frog', 'format' => FORMAT_HTML),
+            1 => array('text' => 'cat', 'format' => FORMAT_HTML),
+            2 => array('text' => 'newt', 'format' => FORMAT_HTML),
+            3 => array('text' => '', 'format' => FORMAT_HTML));
+
+        $q->subanswers = array(
+            0 => array('text' => 'amphibian', 'format' => FORMAT_HTML),
+            1 => array('text' => 'mammal', 'format' => FORMAT_HTML),
+            2 => array('text' => 'amphibian', 'format' => FORMAT_HTML),
+            3 => array('text' => 'insect', 'format' => FORMAT_HTML),
+        );
+
+        $q->noanswers = 4;
+
+        return $q;
+    }
+
 }
