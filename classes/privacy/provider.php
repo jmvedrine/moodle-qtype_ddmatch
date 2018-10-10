be295292be92d71fdaa24d05c2e969a5752615fe
Paul Damiani <pdamiani1996@gmail.com>
2018-10-10 16:41:04 +1100

    Fix namespace

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace format_tabtopics\privacy;
+namespace qtype_ddmatch\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
